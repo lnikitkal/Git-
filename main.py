6035016778f8ce87d85f55601d80a5ce73ff445a
nikitka <nikitazh0@yandex.ru>
2020-12-02 20:31:25 +0300

    last commit

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
 import sys
 from random import randrange
-from PyQt5 import uic  # Импортируем uic
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-class MyWidget(QMainWindow):
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(486, 423)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(10, 10, 93, 28))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 486, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Git и желтые окружности"))
+        self.pushButton.setText(_translate("MainWindow", "Создать круг"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)  # Загружаем дизайн
+        self.setupUi(self)
         self.flag = False
         self.pushButton.clicked.connect(self.run)
         # Обратите внимание: имя элемента такое же как в QTDesigner
@@ -27,14 +55,12 @@ class MyWidget(QMainWindow):
             # Завершаем рисование
             qp.end()
 
-
     def draw_flag(self, qp):
         # Задаем кисть
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(randrange(0, 255), randrange(0, 255), randrange(0, 255)))
         # Рисуем прямоугольник заданной кистью
         n = randrange(10, 200)
-        qp.drawEllipse(200 - n // 2, 200 - n//2, n, n)
-
+        qp.drawEllipse(200 - n // 2, 200 - n // 2, n, n)
 
 
 if __name__ == '__main__':
